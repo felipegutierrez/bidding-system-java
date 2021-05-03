b7fa762c395db162e0b99a465d27becca5dbc4f0
Felipe Gutierrez <felipe.o.gutierrez@gmail.com>
2021-05-03 12:25:21 +0200

    first commit

---
+++ src/main/java/org/github/felipegutierrez/biddingsystem/biddingsystem/BiddingsystemApplication.java
@@ -0,0 +1,13 @@
+package org.github.felipegutierrez.biddingsystem.biddingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiddingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiddingsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unit/org/github/felipegutierrez/biddingsystem/biddingsystem/BiddingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.felipegutierrez.biddingsystem.biddingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
