afad398c6e9c52b4da86229b93b076d38aceae91
Felipe Gutierrez <felipe.o.gutierrez@gmail.com>
2021-05-05 17:33:26 +0200

    added unit test

--- src/test/java/intg/org/github/felipegutierrez/biddingsystem/auction/service/BidderServiceIntgTest.java
+++ src/test/java/intg/org/github/felipegutierrez/biddingsystem/auction/service/BidderServiceIntgTest.java
@@ -1,19 +1,18 @@
 package org.github.felipegutierrez.biddingsystem.auction.service;
 
+import org.github.felipegutierrez.biddingsystem.auction.domain.BidRequest;
 import org.github.felipegutierrez.biddingsystem.auction.domain.BidResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.util.MultiValueMap;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
-import java.util.Map;
 import java.util.stream.Stream;
 
-import static org.mockito.ArgumentMatchers.anyMap;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
 class BidderServiceIntgTest {
@@ -24,14 +23,36 @@ class BidderServiceIntgTest {
     @Test
     void bidResponseStream() {
 
-//        Mockito.when(bidderService.bidResponseStream(anyString(), anyMap()))
-//                .thenReturn(Stream.of(Flux.just(new BidResponse("1", 2500, "c:2500"))));
-//
-//        var response = bidderService.bidResponseStream("2",
-//                Map.of(
-//                        "c", "5",
-//                        "b", "2"
-//                )
-//        );
+        Mockito.when(bidderService.bidResponseStream(any()))
+                .thenReturn(Stream.of(Flux.just(new BidResponse("1", 2500, "c:2500"))));
+
+        var bidResponseFlux = Flux.fromStream(bidderService.bidResponseStream(new BidRequest()))
+                .flatMap(this::gatherResponses);
+
+        StepVerifier.create(bidResponseFlux)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+    @Test
+    void bidResponseStreamSpecificBidResponse() {
+
+        Mockito.when(bidderService.bidResponseStream(any()))
+                .thenReturn(Stream.of(Flux.just(new BidResponse("1", 2500, "c:2500"))));
+
+        var bidResponseFlux = Flux.fromStream(bidderService.bidResponseStream(new BidRequest()))
+                .flatMap(this::gatherResponses);
+
+        StepVerifier.create(bidResponseFlux)
+                .expectNext(new BidResponse("1", 2500, "c:2500"))
+                .verifyComplete();
+    }
+
+    private Flux<BidResponse> gatherResponses(Flux<BidResponse> bidResponseFlux) {
+        try {
+            return bidResponseFlux;
+        } catch (Exception e) {
+            return Flux.empty();
+        }
     }
 }
